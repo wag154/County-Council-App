a84a20a5fa3165079d8ce83ac661002b51a8a52b
woejaddicor <your@emailadd.com>
2023-03-28 10:55:17 +0100

    Cleaned up user controller/model

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,29 +1,32 @@
-const User = require ("../models/user_model");
+const User = require('../models/user_model');
 
-const create = async(req,res) =>{
-  
-    try{
-      const createUser = await User.create(req.body.username,req.body.password);
-      res.send("Success").status()
-    }
-    catch{console.log("Unable to create new user")}
+async function create(req, res) {
+	try {
+		const createUser = await User.create(req.body.username, req.body.password);
+		res.send('Success').status();
+	} catch {
+		console.log('Unable to create new user');
+	}
 }
 
-const userLogin = async(req,res)=>{
-  try{
-    const getLogin = await User.login(req.body.username,red.body.password);
-    res.send("Success").status()
-  }
-  catch{e=>console.log(e);res.status(400)}
+async function userLogin(req, res) {
+	try {
+		const getLogin = await User.login(req.body.username, red.body.password);
+		res.send('Success').status();
+	} catch {
+		(e) => console.log(e);
+		res.status(400);
+	}
 }
 
-const Remove = async(req,res)=>{
-  try{
-    const getID = await User.getUserByID(req.body.name);
-    const deleted = await User.destroy(getID);
-    res.send(200)
-  }
-  catch{
-    console.log("Unable to remove element")
-  }
+async function remove(req, res) {
+	try {
+		const getID = await User.getUserByID(req.body.name);
+		const deleted = await User.destroy(getID);
+		res.send(200);
+	} catch {
+		console.log('Unable to remove element');
+	}
 }
+
+module.exports = { create, userLogin, remove };
--- server/models/user_model.js
+++ server/models/user_model.js
@@ -7,70 +7,51 @@ class User {
 	}
 
 	static async getUserByID(name) {
-		try {
-			const getID = await db.query(
-				'SELECT UserAccount_id FROM UserAccount WHERE username = $1',
-				[name]
-			);
-			return getID.rows[0];
-		} catch {
-			console.log('Unable to find id');
-		}
+		const getID = await db.query(
+			'SELECT UserAccount_id FROM UserAccount WHERE username = $1',
+			[name]
+		);
+		return getID.rows[0];
 	}
 
 	static async check(name, password) {
-		try {
-			const UsernameCheck = db.query(
-				"SELECT * FROM UserAccount WHERE username = '$1' AND password = '$2'",
-				[name, password]
-			);
-			if (UsernameCheck.rows.length === 0) {
-				return true;
-			} else {
-				return false;
-			}
-		} catch {
-			console.log('Unable to check');
+		const UsernameCheck = db.query(
+			"SELECT * FROM UserAccount WHERE username = '$1' AND password = '$2'",
+			[name, password]
+		);
+		if (UsernameCheck.rows.length === 0) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 
 	static async create(name, password) {
 		const isNewAccount = check(name, password);
 		if (isNewAccount == true) {
-			try {
-				const add = await db.query(
-					'INSERT INTO UserAccount VALUES ($1,$2) RETURNING *;',
-					[name, password]
-				);
-				return add.rows.map((e) => User(e));
-			} catch {
-				console.log('Unable to create an account');
-			}
+			const add = await db.query(
+				'INSERT INTO UserAccount VALUES ($1,$2) RETURNING *;',
+				[name, password]
+			);
+			return add.rows.map((e) => User(e));
 		} else {
 			console.log('Username already exists');
 			return 'Already Exists';
 		}
 	}
 	static async login(name, password) {
-		try {
-			const checker = db.query(
-				'SELECT * FROM userAccount WHERE username = $1 AND password = $2 RETURNING *;',
-				[name, password]
-			);
-			return add.rows.map((e) => User(e));
-		} catch {
-			console.log('Unable to check whether the user info is correct');
-		}
+		const checker = db.query(
+			'SELECT * FROM userAccount WHERE username = $1 AND password = $2 RETURNING *;',
+			[name, password]
+		);
+		return add.rows.map((e) => User(e));
 	}
+
 	async destroy(id) {
-		try {
-			const get = await db.query(
-				'DELETE FROM userAccount WHERE UserAccount_id = $1 RETURNING *;',
-				[id]
-			);
-		} catch {
-			console.log('Unable to delete');
-		}
+		const get = await db.query(
+			'DELETE FROM userAccount WHERE UserAccount_id = $1 RETURNING *;',
+			[id]
+		);
 	}
 }
 module.exports = User;
