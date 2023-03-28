22d98079e6eb620eb8f6914549df5efff2ced83a
farhan3311 <farhansidat3311@gmail.com>
2023-03-28 15:07:07 +0100

    functonalities have been added to events

--- client/assets/js/script.js
+++ client/assets/js/script.js
@@ -1,30 +1,58 @@
 
 const btnEvents = document.getElementById("btn-events")
 btnEvents.addEventListener('click', () => {
-    getEventList()
+        window.location.href = "./assets/views/events.html"
 
-    window.location.href = "./assets/views/events.html"
 })
 
 const btnRecycle = document.getElementById("btn-recycle")
-btnRecycle.addEventListener('click', () => {
-    window.location.href = "./assets/views/recycles.html"
-})
+if(btnRecycle) {
+    btnRecycle.addEventListener('click', () => {
+        window.location.href = "./assets/views/recycles.html"
+    })
+}
 
 const btnJob = document.getElementById("btn-job")
-btnJob.addEventListener('click', () => {
-    window.location.href = "./assets/views/jobs.html"
-})
+if(btnJob) {
+    btnJob.addEventListener('click', () => {
+        window.location.href = "./assets/views/jobs.html"
+    })
+}
+
 async function getEventList() {
 
     // const res = await fetch("")
     // const data = await res.json()
     // return data.result()
 
-    // const eventTitle = document.getElementById("event-title")
-    // eventTitle.innerHTML = "Join new football team"
-    console.log("event list")
-    document.getElementById("event-title").eventTitle.innerHTML = "Join new football team"
-   
+    const eventTitle = document.getElementById("event-title")
+    eventTitle.innerHTML = "Join new football team"   
 }
+//getEventList()
+// const listOfCategories = ["glass","plastic","electronics","paper","metal","ceramic","rubber"]
+// let categoryListIndex = 0;
+// GetByCategory(listOfCategories[categoryListIndex])
+// const DisplayCategories = (data) =>{
+//   data.forEach(e =>{
+//     const btn = document.createElement("button")
+//     btn.style = "Whatever"//add styling here
+//     const btnHeader = document.createElement("h2");
+//     const btnDescription = document.createElement("p1");
+//     btnHeader.textContent = e["ItemName"];
+//     btnDescription.textContent = e["ItemDescription"];
+//     btn.appendChild(btnHeader);
+//     btn.appendChild(btnDescription);
+//   })
+//  $(`${listOfCategories[categoryListIndex]}`).appendChild(btn)
+// }
+// const GetByCategory = async(category) =>{
+// try{
+// const resp = await fetch ("/recycling/category",category)
+// if (resp.ok){
+//   const data = resp.json();
+//   DisplayCategories(data);
+// }}
+// catch {
+//   console.log("Unable to fetch by category")
+// }}
 
