891d8289f288d5c8acb0b7d4525a2f27c05198b4
farhan3311 <farhansidat3311@gmail.com>
2023-03-29 11:18:30 +0100

    code added

--- client/assets/js/event.js
+++ client/assets/js/event.js
@@ -1,18 +1,26 @@
 const eventList = [
-    {"events_id": 1, "event_name": "Marathon", "event_description": "Marathon for charity", "event_place": "Florin", "event_time": "30 March 2023 7 am"},
-    {"events_id": 2, "event_name": "Football Match", "event_description": "Match for charity", "event_place": "Florin", "event_time": "30 April 2023 10 am"}
+    {"events_id": 1, "event_name": "2mile Marathon", "event_description": "Marathon for charity", "event_place": "Florin", "event_time": "30 March 2023 7 am"},
+    {"events_id": 2, "event_name": "Football Match", "event_description": "On local youth demand", "event_place": "Florin", "event_time": "30 April 2023 10 am"},
+    {"events_id": 3, "event_name": "Badminton Match", "event_description": "Match for charity", "event_place": "Florin", "event_time": "15 April 2023 10 am"}
 
 ]
-eventList.forEach(getEventList)
-async function getEventList(event) {
+var EventListCount = 0
+const eventsListDiv = document.getElementById("event-list-div")
+eventList.forEach(event => {
+    var eventsListDivChild = document.querySelector(".event-list-div")
 
-    // const res = await fetch("")
-    // const data = await res.json()
-    // return data.result()
-
+    if (EventListCount > 0) {
+        
+        var eventsListDivClone = eventsListDivChild.cloneNode(true)
+        eventsListDivClone.children[1].innerHTML = event.event_name
+        eventsListDiv.appendChild(eventsListDivClone)
+        
+    }
+    else {
+        EventListCount++
+        eventsListDivChild.children[1].innerHTML = event.event_name
+    }
     
-    document.getElementById("event-title").innerHTML = event.event_name
-    document.getElementById("event-desc").innerHTML = event.event_description  
-    console.log(event)
-}
-//getEventList()
+})
+
+
