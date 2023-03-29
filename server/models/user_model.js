c1e12251b0b207a977f878140624e79006092d5b
wag154 <85699535+wag154@users.noreply.github.com>
2023-03-29 16:17:27 +0100

    Update user_model.js

--- server/models/user_model.js
+++ server/models/user_model.js
@@ -5,7 +5,7 @@ class User {
 		this.username = username;
 		this.password = password;
 	}
-<<<<<<< HEAD
+
 
 	static async getUserByID(name) {
 		const getID = await db.query(
@@ -56,63 +56,4 @@ class User {
 	}
 }
 module.exports = User;
-=======
-  static async getUserByID(name){
-    try{
-      const getID = await db.query("SELECT UserAccount_id FROM UserAccount WHERE username = $1",[name])
-      return  getID.rows[0]
-    }
-    catch{
-      console.log("Unable to find id")
-    }
-  }
-  static async check (name,password){
-    try{
-      const UsernameCheck = db.query("SELECT * FROM UserAccount WHERE username = '$1' AND password = '$2'",[name,password]);
-      if (UsernameCheck.rows.length === 0){
-        return true;
-      }
-      else {
-        return false;
-      }
-    }
-    catch {
-      console.log("Unable to check")
-    }
-  }
-  static async create (name,password){
-    const isNewAccount = check(name,password);
-    if (isNewAccount == true){
-      try{
-        const add = await db.query ("INSERT INTO UserAccount VALUES ($1,$2) RETURNING *;",[name,password])
-        return add.rows.map(e => User(e));
-      }
-      catch {
-        console.log("Unable to create an account")
-      }
-    }
-    else {
-      console.log("Username already exists")
-      return "Already Exists"
-    }
-  }
-  static async login (name,password){
-    try{
-      const checker = db.query("SELECT * FROM UserAccount WHERE username = $1 AND password = $2 RETURNING *;",[name,password])
-      return add.rows.map(e => User(e))
-    }
-    catch {
-      console.log("Unable to check whether the user info is correct")
-    }
-  }
-  async destroy (id){
-    try{
-      const get = await db.query("DELETE FROM UserAccount WHERE UserAccount_id = $1 RETURNING *;",[id])
-    }
-    catch {
-      console.log("Unable to delete")
-    }
-  }
- }
-module.exports = User;
->>>>>>> 2a206a1383aafcba71bff384016a15e2e30f1ee8
+
